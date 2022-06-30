b1e19068c4cf8f4070689c194e4f0bfed2ddfbc4
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-30 12:31:54 -0400

    add user authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -19,6 +19,12 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  # def destroy
+  #   @category = Category.find params[:id]
+  #   @category.destroy
+  #   redirect_to [:admin, :categories], notice: 'Category deleted!'
+  # end
+
   private
 
   def category_params
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,14 @@ Rails.application.routes.draw do
   end
   resources :about, only: [:index]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
