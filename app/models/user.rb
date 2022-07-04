78e7624abc6a9422d5778ec4396348ebc2efba93
Ik Sung Choi <ikchoi0@gmail.com>
2022-07-04 16:33:03 -0400

    https://web.compass.lighthouselabs.ca/days/w10d1/activities/445

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+
   has_secure_password
+
+  has_many :reviews
+
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
+
+
+  def self.authenticate_with_credentials(email, password)
+    new_email = email.downcase
+    user = User.find_by_email(new_email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+
 end
+
---
+++ spec/generator/user_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'should create valid user' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: "meow"
+
+        )
+      expect(user).to (be_valid)
+    end
+
+    it 'should have password and password confirmation' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: nil
+        )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should not save without unique email' do
+      @user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password_digest: "me"
+        )
+      @user2 = User.create(
+        name: "Paige",
+        email: "paige@PAIGE.com",
+        password_digest: "me"
+        )
+    expect(@user2).to_not be_valid
+    end
+
+    it 'should have matching password and password confirmation' do
+      password = User.create(
+        name: "Paige",
+        email: "paigekato@gmail.com",
+        password: "meow",
+        password_confirmation: "moo"
+        )
+      expect(password).to_not be_equal(password.password_confirmation)
+    end
+
+    it 'should have name to be a valid user' do
+      user = User.create(
+        name: nil,
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: "meow")
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should have a password with a minimum length of 3' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "ah",
+        password_confirmation: "ah"
+        )
+      expect(user).to_not be_valid
+    end
+  end
+end
